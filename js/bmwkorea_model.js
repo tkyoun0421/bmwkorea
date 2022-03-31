2588250d3c629c48582cb7453be68f36eaca29cf
tkyoun0421 <tkyoun0421@naver.com>
2022-03-31 17:58:43 +0900

    2022-03-31

--- js/bmwkorea_model.js
+++ js/bmwkorea_model.js
@@ -1,8 +1,49 @@
 $(function(){
-    $('.artRight>ul.images>li.image').click(function(){        
-        var index = $(this).index();
-        $('.artLeft>.artView').css({
-            marginTop: (index + 1) * -520 + 'px'
+    var artGroup = $('.artGroup');
+    var thumbGroup = $('.thumbGroup');
+    var btns = $('.thumbView>.btns');
+    thumbGroup.find('.article:last').prependTo(thumbGroup);
+    thumbGroup.css({
+        marginLeft : '-34%'
+    });
+    artGroup.find('.article:last').prependTo(artGroup);
+    artGroup.css({
+        marginLeft : '-100%'
+    })
+    btns.find('.btn:first').click(function(){
+        thumbGroup.stop().animate({
+            marginLeft : '0'
+        },500,function(){
+            thumbGroup.find('.article:last').prependTo(thumbGroup);
+            thumbGroup.css({
+                marginLeft : '-34%'
+            });
+        });
+        artGroup.stop().animate({
+            marginLeft : '0'
+        },500,function(){
+            artGroup.find('.article:last').prependTo(artGroup);
+            artGroup.css({
+                marginLeft : '-100%'
+            });
+        });
+    });
+    btns.find('.btn:last').click(function(){
+        thumbGroup.stop().animate({
+            marginLeft : '-68%'
+        },500,function(){
+            thumbGroup.find('.article:first').appendTo(thumbGroup);
+            thumbGroup.css({
+               marginLeft : '-34%'
+           }); 
+        });
+        artGroup.stop().animate({
+            marginLeft : '-200%'
+        },500,function(){
+            artGroup.find('.article:first').appendTo(artGroup);
+            artGroup.css({
+                marginLeft : '-100%'
+            });
         });
     });
 });
