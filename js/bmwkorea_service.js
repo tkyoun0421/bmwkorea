f6356f0e4190d993fb3b5df34bf5356afc4f569e
tkyoun0421 <tkyoun0421@naver.com>
2022-04-11 17:59:10 +0900

    2022-04-11

--- js/bmwkorea_service.js
+++ js/bmwkorea_service.js
@@ -1,14 +1,4 @@
 $(function(){
-    //배너1
-    var bnrLi = $('.paging>li');
-    bnrLi.click(function(){
-        bnrLi.removeClass('show');
-        $(this).addClass('show');
-        var bnrGroupSlide = $(this).index();
-        $('.bnrGroup').css({
-            marginLeft: bnrGroupSlide * -100 + 'vw'
-        });
-    });
     var nav = $('.menu');
     nav.find('li>i:eq(1)').css({
         display: 'none'
